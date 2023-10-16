dc300604479dc22832604f95899c153780efd897
Naksha2209 <84528961+Naksha2209@users.noreply.github.com>
2023-10-16 13:42:07 +0530

    Create pattern1.py

---
+++ pattern1.py
@@ -0,0 +1,11 @@
+class Solution {
+
+    void printTriangle(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+        System.out.println();
+        }
+    }
+}
