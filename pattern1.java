8edc586c2dc3af3ad646a7de530f641ad9d27545
Naksha2209 <84528961+Naksha2209@users.noreply.github.com>
2023-10-18 22:28:43 +0530

    Create pattern1.java (#25)
    * Create pattern1.java
    
    * Create pattern3.java

---
+++ pattern1.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    void printTriangle(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+        System.out.println();
+        }
+    }
+}
+
+Output:
+1
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
---
+++ pattern3.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    void printTriangle(int n) {
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+        System.out.println();
+        }
+    }
+}
+
+Output:
+1
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
